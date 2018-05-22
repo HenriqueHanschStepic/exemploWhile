ab2e37f4bca858e614361ba118a7dcc7d7c66c4b
Henrique Hänsch Stepic <38055978+HenriqueHanschStepic@users.noreply.github.com>
2018-05-22 18:58:15 -0300

    Create exemplo2.cpp

---
+++ exemplo2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	
+	int number;
+	unsigned long long factorial;
+	printf("Entre um inteiro: ");
+	scanf("%d",&number);
+	
+	factorial = 1;
+	while(number > 0){
+		factorial *= number; --number;
+	}
+	printf("Fatorial: %llu", factorial);
+}
